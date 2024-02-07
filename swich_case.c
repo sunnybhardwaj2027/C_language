da1340d89fbadd11d2c99fc173d7e768676dcfc0
sunnybhardwaj2027 <sunnybhardwaj7549@gmail.com>
2024-02-07 22:16:55 +0530

    i have done few question related to switch case in c language

---
+++ swich_case.c
@@ -0,0 +1,82 @@
+// Q1--Write a C program to print day of week name using switch case
+/*
+#include <stdio.h>
+
+int main()
+{
+    int week;
+
+    printf("Enter Week number(1-7) : ");
+    scanf("%d",&week);
+
+    switch(week){
+        case 1:
+            printf("Monday");
+            break;
+        case 2:
+            printf("Tuesday");
+            break;
+        case 3:
+            printf("Wednesday");
+            break;
+        case 4:
+            printf("Thursday");
+            break;
+        case 5:
+            printf("Friday");
+            break;
+        case 6:
+            printf("Saturday");
+            break;
+        case 7:
+            printf("Sunday");
+            break;
+        default:
+            printf("Invalid input! Please enter week number between 1-7.");
+
+    }
+    return 0;
+}
+*/
+
+// Q2--Write a C program print total number of days in a month using switch case.
+/*
+#include <stdio.h>
+
+int main()
+{
+    int month;
+
+    printf("Enter month number(1-12): ");
+    scanf("%d",&month);
+
+    switch(month){
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            printf("31 days");
+            break;
+        
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            printf("30 days");
+            break;
+
+        case 2:
+            printf("28/29 days");
+            break;
+
+        default:
+            printf("Invalid input! please enter month number between(1-12).");
+    }
+    return 0;
+}
+*/
+
+Q3--
