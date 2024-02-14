6d82899a3e8f1a2f848fcc3149992079c85e0ec0
sunnybhardwaj2027 <sunnybhardwaj7549@gmail.com>
2024-02-14 21:34:12 +0530

    i have done some more question  on matrix in c laguage

--- matrix.c
+++ matrix.c
@@ -342,6 +342,439 @@ int main()
 */
 
 // Q9--Write a C program to interchange diagonals of a matrix
+/*
+#include <stdio.h>
 
+int main()
+{
+    int size, row, col;
+
+    printf("Enter size of the matrix : ");
+    scanf("%d",&size);
+
+    int matrix[size][size];
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            scanf("%d",&matrix[row][col]);
+        }
+        printf("\n");
+    }
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            if(row==col){
+                int temp=matrix[row][col];
+                matrix[row][col]=matrix[row][size-1-row];
+                matrix[row][size-1-row]=temp;
+            }
+        }
+    }
+
+    printf("\nmatrix after diagonals interchanged : \n");
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            printf("%d\t",matrix[row][col]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+*/
+
+// Q10--Write a C program to find upper triangular matrix
+/*
+#include <stdio.h>
+
+int main()
+{
+    int size, row, col;
+
+    printf("Enter size of matrix : ");
+    scanf("%d",&size);
+
+    printf("Enter elements of matrix : \n");
+
+    int matrix[size][size];
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            scanf("%d",&matrix[row][col]);
+        }
+        printf("\n");
+    }
+
+    int Isupper=1;
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            if((row>col) && matrix[row][col]!=0){
+                Isupper=0;
+                break;
+            }
+        }
+    }
+
+
+    if(Isupper==1){
+        printf("Input matrix is upper triangular matrix.");
+    }
+    else{
+        printf("Input matrix is not upper triangular matrix.");
+    }
+
+
+    return 0;
+}
+*/
+
+// Q11-Write a C program to find lower triangular matrix
+/*
+#include <stdio.h>
+
+int main()
+{
+    int size, row, col;
+
+    printf("Enter size of matrix : ");
+    scanf("%d",&size);
+
+    printf("Enter elements of matrix : \n");
+
+    int matrix[size][size];
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            scanf("%d",&matrix[row][col]);
+        }
+        printf("\n");
+    }
+
+    int Islower=1;
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            if((row<col) && matrix[row][col]!=0){
+                Islower=0;
+                break;
+            }
+        }
+    }
+
+
+    if(Islower==1){
+        printf("Input matrix is lower triangular matrix.");
+    }
+    else{
+        printf("Input matrix is not lower triangular matrix.");
+    }
+
+
+    return 0;
+}
+*/
+
+// Q12--Write a C program to find sum of upper triangular matrix
+/*
+#include <stdio.h>
+
+int main()
+{
+    int size, row, col;
+
+    printf("Enter size of matrix : ");
+    scanf("%d",&size);
+
+    int matrix[size][size];
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            scanf("%d",&matrix[row][col]);
+        }
+        printf("\n");
+    }
+
+    int sum = 0;
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            if(row<col){
+                sum+=matrix[row][col];
+            }
+        }
+    }
+
+    printf("\nsum of upper triangular matrix = %d",sum);
+    return 0;
+}
+*/
+
+// Q13--Write a C program to find sum of lower triangular matrix
+/*
+#include <stdio.h>
+
+int main()
+{
+    int size, row, col;
+
+    printf("Enter size of matrix : ");
+    scanf("%d",&size);
+
+    int matrix[size][size];
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            scanf("%d",&matrix[row][col]);
+        }
+        printf("\n");
+    }
+
+    int sum = 0;
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            if(row>col){
+                sum+=matrix[row][col];
+            }
+        }
+    }
+
+    printf("\nsum of lower triangular matrix = %d",sum);
+    return 0;
+}
+*/
+
+// Q14--Write a C program to find transpose of a matrix
+/*
+#include <stdio.h>
+
+int main()
+{
+    int size, row, col;
+
+    printf("Enter size of matrix : ");
+    scanf("%d",&size);
+
+    int Original_matrix[size][size], Transpose_matrix[size][size];
+
+    printf("Enter elements of matrix :\n");
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            scanf("%d",&Original_matrix[row][col]);
+        }
+        printf("\n");
+    }
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            Transpose_matrix[row][col]=Original_matrix[col][row];
+        }
+    }
+
+    printf("Transpose matrix :\n");
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            printf("%d\t",Transpose_matrix[row][col]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+*/
+
+// Q15--Write a C program to find determinant of a matrix
+/*
+#include <stdio.h>
+
+int main()
+{
+    int matrix[3][3], row, col;
+
+    for(row=0;row<3;row++){
+        for(col=0;col<3;col++){
+            scanf("%d",&matrix[row][col]);
+        }
+        printf("\n");
+    }
+
+    int a, b, c, d, e, f, g, h, i;
+    a=matrix[0][0];
+    b=matrix[0][1];
+    c=matrix[0][2];
+    d=matrix[1][0];
+    e=matrix[1][1];
+    f=matrix[1][2];
+    g=matrix[2][0];
+    h=matrix[2][1];
+    i=matrix[2][2];
+
+    printf("Determinant = %d",a*(e*i-f*h)-b*(d*i-f*g)+c*(d*h-e*g));
+    return 0;
+}
+*/
+
+// Q16--Write a C program to check Identity matrix
+/*
+#include <stdio.h>
+
+int main()
+{
+    int size, row, col;
+
+    printf("Enter size of matrix: ");
+    scanf("%d",&size);
+
+    int matrix[size][size];
+
+    printf("Enter elements of matrix : \n");
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            scanf("%d",&matrix[row][col]);
+        }
+        printf("\n");
+    }
+
+    int Is_identity=1;
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            if(row==col){
+                if(matrix[row][col]!=1){
+                    Is_identity=0;
+                    break;
+                }
+            }
+            else{
+                if(matrix[row][col]!=0){
+                    Is_identity=0;
+                    break;
+                }
+            }
+        }
+    }
+
+    if(Is_identity==1){
+        printf("\nGiven matrix is an Identity matrix\n");
+        for(row=0;row<size;row++){
+            for(col=0;col<size;col++){
+                printf("%d\t",matrix[row][col]);
+            }
+            printf("\n");
+        }
+    }
+    else{
+        printf("\nGiven matrix is not an Identity matrix\n");
+    }
+    return 0;
+}
+*/
+
+// Q17--Write a C program to check Sparse matrix
+/*
+#include <stdio.h>
+
+int main()
+{
+    int i, j, row, col;
+
+    printf("Enter number of rows: ");
+    scanf("%d",&row);
+
+    printf("Enter number of column: ");
+    scanf("%d",&col);
+
+    int matrix[row][col];
+
+    printf("Enter elements of (%d * %d)matrix\n",row,col);
+
+    for(i=0;i<row;i++){
+        for(j=0;j<col;j++){
+            scanf("%d",&matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    int total=0;
+    for(i=0;i<row;i++){
+        for(j=0;j<col;j++){
+            if(matrix[i][j]==0){
+                total++;
+            }
+        }
+    }
+
+    if(total>((row*col)/2)){
+        printf("\nGiven matrix is sparse matrix:\n");
+        for(i=0;i<row;i++){
+            for(j=0;j<col;j++){
+                printf("%d\t",matrix[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    else{
+        printf("Given matrix is not sparse matrix:\n");
+    }
+    return 0;
+}
+*/
+
+// Q18--Write a C program to check Symmetric matrix
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    int size, row, col;
+
+    printf("Enter size of matrix: ");
+    scanf("%d",&size);
+
+    int matrix1[size][size], matrix2[size][size];
+
+    printf("Enter enlements of matrix:\n");
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            scanf("%d",&matrix1[row][col]);
+        }
+        printf("\n");
+    }
+
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            matrix2[row][col]=matrix1[col][row];
+        }
+    }
+
+    int Is_symmetric = 1;
+    for(row=0;row<size;row++){
+        for(col=0;col<size;col++){
+            if(matrix1[row][col]!=matrix2[row][col]){
+                Is_symmetric=0;
+                break;
+            }
+        }
+    }
+
+    if(Is_symmetric==1){
+        printf("\nGiven matrix is symmetric\n");
+        for(row=0;row<size;row++){
+            for(col=0;col<size;col++){
+                printf("%d\t",matrix1[row][col]);
+            }
+            printf("\n");
+        }
+    }
+    else{
+        printf("\nGiven matrix is not symmetric\n");
+    }
+    return 0;
+}
+*/
 
 
